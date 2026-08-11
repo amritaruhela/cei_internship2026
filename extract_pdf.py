66851dff2ca14765d6bb859ad88b88755e73696a
Amrita Ruhela <amritaruhela2255@gmail.com>
2026-08-11 23:05:21 +0530

    Delete extract_pdf.py

--- extract_pdf.py
+++
@@ -1,9 +0,0 @@
-import pdfplumber
-
-with pdfplumber.open(r'C:\Users\amrit\OneDrive\Desktop\project\Cross-System Data Drift & Trust Monitoring Platform.pdf') as pdf:
-    for i, page in enumerate(pdf.pages):
-        print(f'=== PAGE {i+1} ===')
-        text = page.extract_text()
-        if text:
-            print(text)
-        print()
